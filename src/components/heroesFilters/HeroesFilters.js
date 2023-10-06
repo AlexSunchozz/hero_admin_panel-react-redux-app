f7c09715ec684813607ec64cccfc048e097693ed
AlexandrRepkin <arepkin96@mail.ru>
2023-10-06 15:35:49 +0300

    combineREducers and fix bags

--- src/components/heroesFilters/HeroesFilters.js
+++ src/components/heroesFilters/HeroesFilters.js
@@ -1,75 +1,64 @@
-// Задача для этого компонента:
-// Фильтры должны формироваться на основании загруженных данных
-// Фильтры должны отображать только нужных героев при выборе
-// Активный фильтр имеет класс active
-// Изменять json-файл для удобства МОЖНО!
-// Представьте, что вы попросили бэкенд-разработчика об этом
+import {useHttp} from '../../hooks/http.hook';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import classNames from 'classnames';
 
-import { useHttp } from "../../hooks/http.hook";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import classNames from "classnames";
-
-import {
-  filtersFetching,
-  filtersFetched,
-  filtersFetchingError,
-  activeFilterChanged,
-} from "../../actions";
-import Spinner from "../spinner/Spinner";
+import { filtersFetching, filtersFetched, filtersFetchingError, activeFilterChanged } from '../../actions';
+import Spinner from '../spinner/Spinner';
 
 const HeroesFilters = () => {
-  const { request } = useHttp();
-  const dispatch = useDispatch();
-  const { filters, filtersLoadingStatus, activeFilter } = useSelector(
-    (state) => state
-  );
 
-  useEffect(() => {
-    dispatch(filtersFetching());
-    request("http://localhost:3001/filters")
-      .then((data) => dispatch(filtersFetched(data)))
-      .catch(() => dispatch(filtersFetchingError()));
-  }, []);
+    const {filters, filtersLoadingStatus, activeFilter} = useSelector(state => state.filters);
+    const dispatch = useDispatch();
+    const {request} = useHttp();
 
-  if (filtersLoadingStatus === "loading") {
-    return <Spinner />;
-  } else if (filtersLoadingStatus === "error") {
-    return <h5 className="text-center mt-5">Ошибка загрузки</h5>;
-  }
+    useEffect(() => {
+        dispatch(filtersFetching());
+        request("http://localhost:3001/filters")
+            .then(data => dispatch(filtersFetched(data)))
+            .catch(() => dispatch(filtersFetchingError()))
 
-  const renderFilters = (filters) => {
-    if (filters.length === 0)
-      return <h5 className="text-center mt-5">Ошибка загрузки</h5>;
+        // eslint-disable-next-line
+    }, []);
 
-    return filters.map(({ name, className, label }) => {
-      const btnClass = classNames("btn", className, {
-        active: name === activeFilter,
-      });
+    if (filtersLoadingStatus === "loading") {
+        return <Spinner/>;
+    } else if (filtersLoadingStatus === "error") {
+        return <h5 className="text-center mt-5">Ошибка загрузки</h5>
+    }
 
-      return (
-        <button
-          className={btnClass}
-          key={name}
-          id={name}
-          onClick={() => activeFilterChanged(name)}
-        >
-          {label}
-        </button>
-      );
-    });
-  };
+    const renderFilters = (arr) => {
+        if (arr.length === 0) {
+            return <h5 className="text-center mt-5">Фильтры не найдены</h5>
+        }
 
-  const elements = renderFilters(filters);
+        return arr.map(({name, className, label}) => {
 
-  return (
-    <div className="card shadow-lg mt-4">
-      <div className="card-body">
-        <p className="card-text">Отфильтруйте героев по элементам</p>
-        <div className="btn-group">{elements}</div>
-      </div>
-    </div>
-  );
-};
+            const btnClass = classNames('btn', className, {
+                'active': name === activeFilter
+            });
+            
+            return <button 
+                        key={name} 
+                        id={name} 
+                        className={btnClass}
+                        onClick={() => dispatch(activeFilterChanged(name))}
+                        >{label}</button>
+        })
+    }
+
+    const elements = renderFilters(filters);
+
+    return (
+        <div className="card shadow-lg mt-4">
+            <div className="card-body">
+                <p className="card-text">Отфильтруйте героев по элементам</p>
+                <div className="btn-group">
+                    {elements}
+                </div>
+            </div>
+        </div>
+    )
+}
 
 export default HeroesFilters;
