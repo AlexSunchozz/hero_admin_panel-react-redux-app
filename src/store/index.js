88d4af8429c051a628cf49ce3eafd92f6f1aab69
AlexandrRepkin <arepkin96@mail.ru>
2023-10-07 13:30:08 +0300

    redux-thunk middleware filters and heroes

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,15 @@
 import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
 
-const stringMiddleware = ({dispatch, getState}) => (dispatch) => (action) => {
+const stringMiddleware = () => (next) => (action) => {
     if (typeof action === 'string') {
-        return dispatch({
+        return next({
             type: action
-        });
-    } 
-    return dispatch(action);
+        })
+    }
+    return next(action)
 };
 
 const enhancer = (createStore) => (...args) => {
@@ -26,15 +27,9 @@ const enhancer = (createStore) => (...args) => {
     return store;
 };
 
-const store = createStore(
-                combineReducers({heroes, filters}), 
-                compose(applyMiddleware(stringMiddleware),
-                        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-                // compose(
-                //     enhancer,
-                //     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-                // )
+const store = createStore( 
+                combineReducers({heroes, filters}),
+                compose(applyMiddleware(ReduxThunk, stringMiddleware))
                 );
 
 export default store;
-
